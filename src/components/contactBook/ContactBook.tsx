e3233a4e7c00121aabd4fb0e4667e3146c227bc5
mahimag <mahimag>
2022-09-12 18:42:51 +0545

    fixes update

--- src/components/contactBook/ContactBook.tsx
+++ src/components/contactBook/ContactBook.tsx
@@ -5,61 +5,61 @@ import { HeartOutlined, HeartFilled } from "@ant-design/icons";
 import "./ContactBook.css";
 import { Contact } from "../../interfaces/Contact";
 import axios from "axios";
-import { Link } from "react-router-dom";
-
-const onDeleteHandler = async (id: number) => {
-  await axios.delete(`/contacts/${id}`);
-  window.location.reload();
-};
-
-const columns: ColumnsType<Contact> = [
-  {
-    title: "Photo",
-    dataIndex: "photo",
-    key: "photo",
-    render: (url) => (
-      <img
-        style={{
-          width: "80px",
-          height: "80px",
-        }}
-        src={url}
-        alt={url}
-      />
-    ),
-  },
-  { title: "Name", dataIndex: "firstname", key: "firstname" },
-  { title: "Number", dataIndex: "number", key: "number" },
-  {
-    title: "Favorite",
-    dataIndex: "isFav",
-    key: "isFav",
-    render: (_, record) => {
-      return record.isFav ? <HeartFilled /> : <HeartOutlined />;
-    },
-  },
-  {
-    title: "Action",
-    dataIndex: "",
-    key: "x",
-    render: (_, record) => (
-      <div id="actions">
-        <Button
-          onClick={() => {
-            onDeleteHandler(+record.id);
-          }}
-        >
-          Delete
-        </Button>
-        <Button>
-          <Link to={`/contact/update/${record.id}`}>Update</Link>
-        </Button>
-      </div>
-    ),
-  },
-];
+import { Link, useNavigate } from "react-router-dom";
 
 const ContactBook = () => {
+  const navigate = useNavigate();
+
+  const onDeleteHandler = async (id: number) => {
+    await axios.delete(`/contacts/${id}`);
+    navigate("/");
+  };
+  const columns: ColumnsType<Contact> = [
+    {
+      title: "Photo",
+      dataIndex: "photo",
+      key: "photo",
+      render: (url) => (
+        <img
+          style={{
+            width: "80px",
+            height: "80px",
+          }}
+          src={url}
+          alt={url}
+        />
+      ),
+    },
+    { title: "Name", dataIndex: "firstname", key: "firstname" },
+    { title: "Number", dataIndex: "number", key: "number" },
+    {
+      title: "Favorite",
+      dataIndex: "isFav",
+      key: "isFav",
+      render: (_, record) => {
+        return record.isFav ? <HeartFilled /> : <HeartOutlined />;
+      },
+    },
+    {
+      title: "Action",
+      dataIndex: "",
+      key: "x",
+      render: (_, record) => (
+        <div id="actions">
+          <Button
+            onClick={() => {
+              onDeleteHandler(+record.id);
+            }}
+          >
+            Delete
+          </Button>
+          <Button>
+            <Link to={`/contact/update/${record.id}`}>Update</Link>
+          </Button>
+        </div>
+      ),
+    },
+  ];
   useEffect(() => {
     getData(1);
   }, []);
